c8c81609203c5b909226b7a0cbe49a07d70c2cc2
Tina Ko <tinarchan@gmail.com>
2017-04-07 20:42:03 -0700

    (bug) attempt to change port?

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.get('/auth/facebook/return',
     res.redirect('/');
   });
 
-let port = process.env.PORT;
+let port = process.env.PORT || 1337;
 
 app.listen(port, function() {
   console.log('listening on ' + port + '!');
