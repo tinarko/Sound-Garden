bc67d9d676c229b5159c2a687f6ad7125a142758
Tina Ko <tinarchan@gmail.com>
2017-04-07 19:53:15 -0700

    (bug) add sql heroku configs

--- server/index.js
+++ server/index.js
@@ -45,15 +45,12 @@ app.get('/auth/facebook/return',
     res.redirect('/');
   });
 
-<<<<<<< HEAD
 app.post('/plaid/access_token', requestHandler.plaid.accessToken);
-app.listen(1337, function() {
-  console.log('listening on port 1337!');
-=======
+
 let port = process.env.PORT || 1337;
 
 app.listen(port, function() {
   console.log('listening on ' + port + '!');
->>>>>>> (Deploy) Update files for Heroku
+
 });
 
