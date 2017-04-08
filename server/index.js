26577db72b9f0ebb316a13cf53372c10f7daf18b
Tina Ko <tinarchan@gmail.com>
2017-04-07 21:06:20 -0700

    (bug) change packages

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var passport = require('passport');
+var http = require('http');
+http.createServer(function(req, res) {});
 
 // import passport authentication strategies
 var authentication = require('./authentication');
@@ -48,6 +50,8 @@ app.get('/auth/facebook/return',
 app.post('/plaid/access_token', requestHandler.plaid.accessToken);
 
 let port = process.env.PORT || 1337;
+
+// http.createServer(onRequest).listen(process.env.PORT || 6000);
 
 app.listen(port, function() {
   console.log('listening on ' + port + '!');
